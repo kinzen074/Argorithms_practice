91184f59da4083844ed831b73c75a0ce48f9c282
kinzen074 <zxr_dlut@outlook.com>
2021-02-06 20:43:28 +0900

    v1.0

--- sorts.py
+++ sorts.py
@@ -6,7 +6,7 @@ def insert_sort(arr):
             arr[idx+1]=arr[idx]
             idx-=1
         arr[idx+1]=num
-    print(arr)
+    return arr
 def bb_sort(arr):
     flag=1
     while(flag):
@@ -15,8 +15,8 @@ def bb_sort(arr):
             if(arr[i]>arr[i+1]):
                 arr[i],arr[i+1]=arr[i+1],arr[i]
                 flag=1
-    print(arr)
+    return arr
 if __name__ == '__main__':
     list1 = [5, 2, 3, 1, 4]
-    insert_sort(list1)
-    bb_sort(list1)
+    print(insert_sort(list1))
+    print(bb_sort(list1))
